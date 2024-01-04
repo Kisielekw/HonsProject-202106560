b737cfb6fe994ef046eddb24d1afbf834aead705
Kisielekw <dawkis1@outlook.com>
2024-01-04 02:19:12 +0000

    added DoG into the header file for both cuda and c++

--- Project/ImageProcessingWithCuda/ImageProcessingWithCuda/ImageProcessing.h
+++ Project/ImageProcessingWithCuda/ImageProcessingWithCuda/ImageProcessing.h
@@ -24,6 +24,18 @@ public:
 	/// <param name="height">The height of the image</param>
 	/// <returns></returns>
 	static std::chrono::microseconds Sobel(const unsigned char* imageIn, unsigned char* imageOut, const int width, const int height);
+	/// <summary>
+	/// A Difference of Gaussian edge detection implementation in C++
+	/// </summary>
+	/// <param name="imageIn">The image being blured</param>
+	/// <param name="imageOut">The output image of the Gaussian blure</param>
+	/// <param name="width">The width of the image</param>
+	/// <param name="height">The height of the image</param>
+	/// <param name="sd1">The standard deviation of the first Gaussian blure</param>
+	/// <param name="sd2">The standard deviation of the second Gaussian blure</param>
+	/// <param name="kernalSize">The size of the kernal</param>
+	/// <returns></returns>
+	static std::chrono::microseconds DoG(const unsigned char* imageIn, unsigned char* imageOut, const int width, const int height, const float sd1, const float sd2, const unsigned int kernalSize);
 private:
 	static int Convolution(const unsigned char*, const float*, const int, const int, const int, const int, const unsigned int);
 	static float GaussianFunction2D(const int, const int, const float);
@@ -52,4 +64,16 @@ public:
 	/// <param name="height">The height of the image</param>
 	/// <returns></returns>
 	static std::chrono::microseconds Sobel(const unsigned char* imageIn, unsigned char* imageOut, const int width, const int height);
+	/// <summary>
+	/// A Difference of Gaussian edge detection implementation in CUDA
+	/// </summary>
+	/// <param name="imageIn">The image being blured</param>
+	/// <param name="imageOut">The output image of the Gaussian blure</param>
+	/// <param name="width">The width of the image</param>
+	/// <param name="height">The height of the image</param>
+	/// <param name="sd1">The standard deviation of the first Gaussian blure</param>
+	/// <param name="sd2">The standard deviation of the second Gaussian blure</param>
+	/// <param name="kernalSize">The size of the kernal</param>
+	/// <returns></returns>
+	static std::chrono::microseconds DoG(const unsigned char* imageIn, unsigned char* imageOut, const int width, const int height, const float sd1, const float sd2, const unsigned int kernalSize);
 };
