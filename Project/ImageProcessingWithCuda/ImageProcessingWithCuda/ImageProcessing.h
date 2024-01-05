ecdd9bbc90053b9cfba94bbf53ccfe9be74ba34d
Kisielekw <dawkis1@outlook.com>
2024-01-05 00:38:54 +0000

    Added KmeansGrayscale method to both cpp and cuda classes

--- Project/ImageProcessingWithCuda/ImageProcessingWithCuda/ImageProcessing.h
+++ Project/ImageProcessingWithCuda/ImageProcessingWithCuda/ImageProcessing.h
@@ -24,6 +24,18 @@ public:
 	/// <param name="height">The height of the image</param>
 	/// <returns></returns>
 	static std::chrono::microseconds Sobel(const unsigned char* imageIn, unsigned char* imageOut, const int width, const int height);
+	/// <summary>
+	/// A K-Means implementation on a grayscale image in C++
+	/// </summary>
+	/// <param name="imageIn">The image geing segmented</param>
+	/// <param name="imageOut">The output image of the K-Mean algorithm</param>
+	/// <param name="width">The width of the image</param>
+	/// <param name="height">The height of the image</param>
+	/// <param name="k">The number of clusters</param>
+	/// <param name="iterationNum">The number of itterations to be preformed</param>
+	/// <returns>The time it takes to run the algorithm in microseconds</returns>
+	static std::chrono::microseconds KMeansGrayscale(const unsigned char* imageIn, unsigned char* imageOut, const int width, const int height, const int k, const int iterationNum);
+
 private:
 	static int Convolution(const unsigned char*, const float*, const int, const int, const int, const int, const unsigned int);
 	static float GaussianFunction2D(const int, const int, const float);
@@ -52,4 +64,15 @@ public:
 	/// <param name="height">The height of the image</param>
 	/// <returns></returns>
 	static std::chrono::microseconds Sobel(const unsigned char* imageIn, unsigned char* imageOut, const int width, const int height);
+	/// <summary>
+	/// A K-Means implementation on a grayscale image in CUDA
+	/// </summary>
+	/// <param name="imageIn">The image geing segmented</param>
+	/// <param name="imageOut">The output image of the K-Mean algorithm</param>
+	/// <param name="width">The width of the image</param>
+	/// <param name="height">The height of the image</param>
+	/// <param name="k">The number of clusters</param>
+	/// <param name="iterationNum">The number of itterations to be preformed</param>
+	/// <returns>The time it takes to run the algorithm in microseconds</returns>
+	static std::chrono::microseconds KMeansGrayscale(const unsigned char* imageIn, unsigned char* imageOut, const int width, const int height, const int k, const int iterationNum);
 };
