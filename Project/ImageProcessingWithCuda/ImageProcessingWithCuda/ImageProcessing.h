1178d8d364815e61fa92a7b3d9345083368436c4
Kisielekw <dawkis1@outlook.com>
2024-01-05 00:40:49 +0000

    added return descriptions to methods xml comments

--- Project/ImageProcessingWithCuda/ImageProcessingWithCuda/ImageProcessing.h
+++ Project/ImageProcessingWithCuda/ImageProcessingWithCuda/ImageProcessing.h
@@ -13,7 +13,7 @@ public:
 	/// <param name="height">The height of the image</param>
 	/// <param name="sd">The standard deviation of the Gaussian</param>
 	/// <param name="kernalSize">The size of the kernal</param>
-	/// <returns></returns>
+	/// <returns>The time it takes to run the algorithm in microseconds</returns>
 	static std::chrono::microseconds GaussianBlur(const unsigned char* imageIn, unsigned char* imageOut, const int width, const int height, const float sd, const unsigned int kernalSize);
 	/// <summary>
 	/// A Sobel edge detection implementation in C++
@@ -22,7 +22,7 @@ public:
 	/// <param name="imageOut">The image output by the Sobel opperator</param>
 	/// <param name="width">The width of the image</param>
 	/// <param name="height">The height of the image</param>
-	/// <returns></returns>
+	/// <returns>The time it takes to run the algorithm in microseconds</returns>
 	static std::chrono::microseconds Sobel(const unsigned char* imageIn, unsigned char* imageOut, const int width, const int height);
 	/// <summary>
 	/// A K-Means implementation on a grayscale image in C++
@@ -53,7 +53,7 @@ public:
 	/// <param name="height">The height of the image</param>
 	/// <param name="sd">The standard deviation of the Gaussian</param>
 	/// <param name="kernalSize">The size of the kernal</param>
-	/// <returns></returns>
+	/// <returns>The time it takes to run the algorithm in microseconds</returns>
 	static std::chrono::microseconds GaussianBlur(const unsigned char* imageIn, unsigned char* imageOut, const int width, const int height, const float sd, const unsigned int kernalSize);
 	/// <summary>
 	/// A Sobel edge detection implementation in CUDA
@@ -62,7 +62,7 @@ public:
 	/// <param name="imageOut">The image output by the Sobel opperator</param>
 	/// <param name="width">The width of the image</param>
 	/// <param name="height">The height of the image</param>
-	/// <returns></returns>
+	/// <returns>The time it takes to run the algorithm in microseconds</returns>
 	static std::chrono::microseconds Sobel(const unsigned char* imageIn, unsigned char* imageOut, const int width, const int height);
 	/// <summary>
 	/// A K-Means implementation on a grayscale image in CUDA
