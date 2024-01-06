8dda8ec64b1a72545ff86f2185a2763f06f35b21
Kisielekw <dawkis1@outlook.com>
2024-01-06 00:18:15 +0000

    Edited cli to alow both display and benchmarking of K-Means as well as sobel

--- Project/ImageProcessingWithCuda/ImageProcessingWithCuda/Main.cpp
+++ Project/ImageProcessingWithCuda/ImageProcessingWithCuda/Main.cpp
@@ -6,15 +6,66 @@
 #include <iostream>
 #include <filesystem>
 
-void SobelBenchmark(const std::string &testFile, const int iterationNum, const int startSize, const int endSize)
+enum Algorithms {
+	Sobel,
+	KMeans
+};
+
+std::string* GetImageFile()
 {
+	std::string directory_path = "Standard_Test_Images\\";
+	std::vector<std::string> fileNames;
+	
+	int count = 1;
+	for (const std::filesystem::directory_entry& entry : std::filesystem::directory_iterator(directory_path))
+	{
+		if (entry.is_regular_file() && entry.path().extension() == ".tif")
+		{
+			std::cout << count++ << ". " << entry.path().string() << std::endl;
+			fileNames.push_back(entry.path().string());
+		}
+	}
+
+	int input = 0;
+	std::cin >> input;
+	return new std::string(fileNames[input - 1]);
+}
+
+Algorithms GetAlgorithm()
+{
+	int userInput = 0;
+	do
+	{
+		std::cout << "Select option:" << std::endl
+			<< "1. Sobel" << std::endl
+			<< "2. K-Means" << std::endl;
+		std::cin >> userInput;
+	} while (userInput < 1 || userInput > 2);
+
+	return static_cast<Algorithms>(userInput - 1);
+}
+
+void SobelBenchmark(const std::string& testFile)
+{
+	//User inputs for the benchmark test
+	int startSize = 0;
+	int endSize = 0;
+	int iterationNum = 0;
+
+	std::cout << "Enter the starting image size: ";
+	std::cin >> startSize;
+	std::cout << "Enter the ending image size: ";
+	std::cin >> endSize;
+	std::cout << "Enter the number of iterations: ";
+	std::cin >> iterationNum;
+
 	std::ofstream output(testFile + ".csv");
 
 	output << "Image Size,CPU Time,CUDA Time" << std::endl;
 
 	for (int size = startSize; size <= endSize; size++)
 	{
-		std::cout << '\r' << "percentage complete: " << (size - startSize) * 100 / (endSize - startSize) << "%";
+		std::cout << '\r' << "percentage complete: " << (size - startSize + 1) * 100 / (endSize - startSize + 1) << "%";
 
 		output << (size * size) << ",";
 
@@ -46,10 +97,97 @@ void SobelBenchmark(const std::string &testFile, const int iterationNum, const i
 	std::cout << std::endl << "Benchmark finished" << std::endl;
 }
 
-void DisplaySobelImage(const std::string &imagePath)
+void KMeansBenchmark(const std::string& testFile)
 {
-	cv::Mat image = cv::imread(imagePath, cv::IMREAD_GRAYSCALE);
+	int benchmarkIterationNum = 0;
+	int imageSizeStart = 0;
+	int imageSizeEnd = 0;
+	int kStart = 0;
+	int kEnd = 0;
+	int iterationNumStart = 0;
+	int iterationNumEnd = 0;
 
+	std::cout << "Enter the number of iterations to be preformed for each benchmark: ";
+	std::cin >> benchmarkIterationNum;
+	std::cout << "Enter the starting image size: ";
+	std::cin >> imageSizeStart;
+	std::cout << "Enter the ending image size: ";
+	std::cin >> imageSizeEnd;
+	std::cout << "Enter the starting number of clusters: ";
+	std::cin >> kStart;
+	std::cout << "Enter the ending number of clusters: ";
+	std::cin >> kEnd;
+	std::cout << "Enter the starting number of iterations in the algorithm: ";
+	std::cin >> iterationNumStart;
+	std::cout << "Enter the ending number of iterations in the algorithm: ";
+	std::cin >> iterationNumEnd;
+
+	std::ofstream output(testFile + ".csv");
+
+	output << "Image Size,Number of Clusters,Number of Iterations,CPU Time,CUDA Time" << std::endl;
+
+	int count = 0;
+
+	for (int imageSize = imageSizeStart; imageSize <= imageSizeEnd; imageSize++)
+	{
+		cv::Mat image = cv::Mat(imageSize, imageSize, CV_8UC1);
+
+		cv::randn(image, 128, 20);
+
+		unsigned char* cppImageData = new unsigned char[image.cols * image.rows];
+		unsigned char* cudaImageData = new unsigned char[image.cols * image.rows];
+
+		for (int k = kStart; k <= kEnd; k++)
+		{
+			for (int iterationNum = iterationNumStart; iterationNum <= iterationNumEnd; iterationNum++)
+			{
+				count++;
+				std::cout << '\r' << "percentage complete: " << (count * 100) / ((imageSizeEnd - imageSizeStart + 1) * (kEnd - kStart + 1) * (iterationNumEnd - iterationNumStart + 1)) << "%";
+
+				output << (imageSize * imageSize) << "," << k << "," << iterationNum << ",";
+
+				long cppDurationSum = 0;
+				long cudaDurationSum = 0;
+
+				for (int i = 0; i < benchmarkIterationNum; i++)
+				{
+					std::chrono::microseconds cppDuration = cppImageProcessing::KMeansGrayscale(image.data, cppImageData, image.cols, image.rows, k, iterationNum);
+					std::chrono::microseconds cudaDuration = cudaImageProcessing::KMeansGrayscale(image.data, cudaImageData, image.cols, image.rows, k, iterationNum);
+
+					cppDurationSum += cppDuration.count();
+					cudaDurationSum += cudaDuration.count();
+				}
+
+				output << cppDurationSum / benchmarkIterationNum << "," << cudaDurationSum / benchmarkIterationNum << std::endl;
+			}
+		}
+
+		delete[] cudaImageData;
+		delete[] cppImageData;
+	}
+
+	std::cout << std::endl << "Benchmark finished" << std::endl;
+}
+
+void SaveImage(cv::Mat& image)
+{
+	std::cout << "Save image? (y/n): ";
+	char input;
+	std::cin >> input;
+
+	if (input == 'y')
+	{
+		std::string saveFile;
+		std::cout << "Enter name of file to save image to with file extension: ";
+		std::cin >> saveFile;
+
+		cv::imwrite(saveFile, image);
+	}
+}
+
+void SobelDisplay(const std::string& imageFile)
+{
+	cv::Mat image = cv::imread(imageFile, cv::IMREAD_GRAYSCALE);
 	cv::Mat cppImage = cv::Mat(image.rows, image.cols, CV_8UC1);
 	cv::Mat cudaImage = cv::Mat(image.rows, image.cols, CV_8UC1);
 
@@ -59,79 +197,83 @@ void DisplaySobelImage(const std::string &imagePath)
 	cv::hconcat(image, cppImage, image);
 	cv::hconcat(image, cudaImage, image);
 
-	cv::imshow("Sobel Comparison", image);
-
+	cv::imshow("Sobel", image);
 	cv::waitKey(0);
-	if(cv::getWindowProperty("Sobel Comparison", cv::WND_PROP_VISIBLE) > 0)
-		cv::destroyWindow("Sobel Comparison");
+	if(cv::getWindowProperty("Sobel", cv::WND_PROP_VISIBLE))
+		cv::destroyWindow("Sobel");
+
+	SaveImage(image);
+}
+
+void KMeansDisplay(const std::string& imageFile)
+{
+	int k = 0;
+	int iterationNum = 0;
+
+	std::cout << "Enter the number of clusters: ";
+	std::cin >> k;
+	std::cout << "Enter the number of iterations: ";
+	std::cin >> iterationNum;
+
+	cv::Mat image = cv::imread(imageFile, cv::IMREAD_GRAYSCALE);
+	cv::Mat cppImage = cv::Mat(image.rows, image.cols, CV_8UC1);
+	cv::Mat cudaImage = cv::Mat(image.rows, image.cols, CV_8UC1);
+
+	cppImageProcessing::KMeansGrayscale(image.data, cppImage.data, image.cols, image.rows, k, iterationNum);
+	cudaImageProcessing::KMeansGrayscale(image.data, cudaImage.data, image.cols, image.rows, k, iterationNum);
+
+	cv::hconcat(image, cppImage, image);
+	cv::hconcat(image, cudaImage, image);
+
+	cv::imshow("K-Means", image);
+	cv::waitKey(0);
+	if (cv::getWindowProperty("K-Means", cv::WND_PROP_VISIBLE))
+		cv::destroyWindow("K-Means");
+
+	SaveImage(image);
 }
 
 int main()
 {
-	std::cout << "Image Processing with CUDA" << std::endl;
-	while (true)
+	std::cout << "Image processing with CUDA" << std::endl;
+	
+	int userInput = 0;
+	do
 	{
-		std::cout << "1. Sobel Benchmark" << std::endl;
-		std::cout << "2. Display Sobel Image" << std::endl;
+		std::cout << "Select option:" << std::endl 
+			<< "1. Benchmark" << std::endl 
+			<< "2. Display Result" << std::endl
+			<< "3. Exit" << std::endl;
+		std::cin >> userInput;
 
-		int input;
-		std::cin >> input;
+		if (userInput == 3)
+			continue;
 
-		if (input == 1)
+		Algorithms algorithm = GetAlgorithm();
+
+		if (userInput == 1)
 		{
-			std::cout << "Enter the number of iterations: ";
-			int iterationNum;
-			std::cin >> iterationNum;
-
-			std::cout << "Enter the start size: ";
-			int startSize;
-			std::cin >> startSize;
-
-			std::cout << "Enter the end size: ";
-			int endSize;
-			std::cin >> endSize;
-
-			std::cout << "Enter the test file name: ";
 			std::string testFile;
+			std::cout << "Enter name of file to save benchmark results to: ";
 			std::cin >> testFile;
 
-			SobelBenchmark(testFile, iterationNum, startSize, endSize);
-			
-			continue;
+			if (algorithm == 0)
+				SobelBenchmark(testFile);
+			else if (algorithm == 1)
+				KMeansBenchmark(testFile);
 		}
-		else if (input == 2)
+		else if (userInput == 2)
 		{
-			std::string directory_path = "Standard_Test_Images\\";
-			std::vector<std::string> file_names;
-			int count = 1;
-			for (const auto& entry : std::filesystem::directory_iterator(directory_path)) {
-				if (entry.is_regular_file() && entry.path().extension() == ".tif") {
-					std::cout << count++ << ". " << entry.path().string() << std::endl;
-					file_names.push_back(entry.path().string());
-				}
-			}
+			std::string* imageFile = GetImageFile();
+			if (algorithm == 0)
+				SobelDisplay(*imageFile);
+			else if (algorithm == 1)
+				KMeansDisplay(*imageFile);
 
-			while (true)
-			{
-				std::cout << "Enter the image number: ";
-				int imageNum;
-				std::cin >> imageNum;
-
-				if (imageNum < 1 || imageNum > file_names.size())
-				{
-					std::cout << "Invalid image number" << std::endl;
-					continue;
-				}
-
-				DisplaySobelImage(file_names[imageNum - 1]);
-				break;
-			}
-
-			continue;
+			delete imageFile;
 		}
 
-		break;
-	}
+	} while (userInput != 3);
 
 	return 0;
 }
